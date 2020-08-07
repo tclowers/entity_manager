5a53f5ab0656ca0184c9ffe30ff0b650f4fb8d0f
Tom Clowers <tclowers@gmail.com>
2020-08-07 11:56:22 -0500

    added jest tests for apollo component

--- packages/frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders book list', () => {
-  const { getByText } = render(<App />);
-  const listElement = getByText(/Books:/i);
-  expect(listElement).toBeInTheDocument();
-});
